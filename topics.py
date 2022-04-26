fdab36e14366f5287f5e370979268090554fec5f
Dominic Payer <payerdom@my.uri.edu>
2022-04-26 12:36:05 -0400

    random topic

---
+++ topics.py
@@ -0,0 +1,13 @@
+
+topics = [
+    "Hash Tables", "POST vs GET", "B-Trees (SQL)", "SQL Macros",
+    "Excel Automation", "Linked Lists", "VSCode Features", 
+    "shortcutfoo.com", "debugging", "Python Automation",
+    "DSA", 
+]
+
+vscode = [
+    "Ctrl + / :comment out code line (or highlight)",
+    "Ctrl + Enter :add newline below current line without breaking line",
+    "Ctrl + p :search for file by name instead of using Explorer",
+]
